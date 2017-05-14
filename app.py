e642097b5b3537b66a5c38f6fac4d6a1206f6efb
namita <namita@PaGaLGuYs-MacBook-Pro.local>
2017-05-14 11:16:39 +0530

    requirements and config file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext import restful
+
+from apis.events_api import RulesAPIHandler
+from models.events import db
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@localhost/cube"
+db.init_app(app)
+
+api.add_resource(RulesAPIHandler, '/api/rules')
+
+if __name__ == '__main__':
+  
+  app.run(debug=True)
+  
